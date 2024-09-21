b6e9b32899849096bf5548b6de0160335a07d783
andresmbd <anmabadi@outlook.com>
2024-09-21 12:25:03 -0500

    if conditionals activity completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,97 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+
+        Console.Write("Enter your grade percentage: ");
+        string letter = "A";
+        string userinput = Console.ReadLine(); 
+      
+        int gradePercentage = int.Parse(userinput);
+        int remain = gradePercentage % 10;
+        string sign = remain.ToString();
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+            if (remain > 3)
+            {  
+                sign = "";
+            }
+            else 
+            {
+                sign = "-";
+            }
+
+
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+            if (remain > 7)
+            {
+                sign = "+";
+            }
+            else if (remain <= 6 && remain >= 3)
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "-";
+            }
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+            if (remain > 7)
+            {
+                sign = "+";
+            }
+            else if (remain <= 6 && remain >= 3)
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "-";
+            }
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+            if (remain > 7)
+            {
+                sign = "+";
+            }
+            else if (remain <= 6 && remain >= 3)
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            letter = "F";
+            sign = "";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"{letter}{sign} Congratulation!! You passed the course.");
+        }
+
+        else 
+        {
+            Console.WriteLine($"{letter}{sign} Sorry you did not pass the course, try next time.");
+        }
+
     }
 }
