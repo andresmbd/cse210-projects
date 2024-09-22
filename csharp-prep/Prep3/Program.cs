cb5ac2cc01cfa3c0f9834be2cf1c4010d58f38e6
andresmbd <anmabadi@outlook.com>
2024-09-22 00:01:15 -0500

    guessing activity completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenarator = new Random();
+
+        int number = randomGenarator.Next(1, 101);
+        // string userNumber;
+        string userGuessed;
+
+        // Console.Write("What is the magic number? ");
+        // userNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(userNumber); 
+
+        Console.Write("What is your guess? ");
+        userGuessed = Console.ReadLine();
+        int guessedNumber = int.Parse(userGuessed);
+
+        while (number != guessedNumber)
+        {
+            if (guessedNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guessedNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            
+            Console.Write("What is your guess? ");
+            userGuessed = Console.ReadLine();
+            guessedNumber = int.Parse(userGuessed); 
+        }
+         Console.WriteLine("You guessed it!");
+        
     }
 }
