ec21657121cbd449867e23f7536607c20df270f0
andresmbd <anmabadi@outlook.com>
2024-09-24 19:23:34 -0500

    last correction made

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,11 +1,11 @@
 public class Resume
 {
-    public string name;
+    public string _name;
     public List<Job>_jobs = new List<Job>();
 
     public void DisplayMyResume()
     {
-        Console.WriteLine($"Name: {name}\nJobs:");
+        Console.WriteLine($"Name: {_name}\nJobs:");
 
         foreach (Job j in _jobs)
         {
